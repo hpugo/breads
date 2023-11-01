e876297ca370fcb241a56237482c7444de524393
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2023-10-31 19:35:14 -0500

    adding code to bread.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose')
+
+
 // DEPENDENCIES
 const express = require('express')
 
@@ -21,3 +24,22 @@ app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
 
+
+// mongoose.connect(
+//     process.env.MONGO_URI,
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     }
+//   )
+//   .then(() => {
+//     console.log('Connected to MongoDB:', process.env.MONGO_URI);
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to MongoDB:', error.message);
+//   });
+
+
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('connected to mongo: ', process.env.MONGO_URI))
