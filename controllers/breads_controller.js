7a450c274ba88c74a7510dfb684aedb6b6543383
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2023-10-31 20:42:40 -0500

    mongooose

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,12 +4,38 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  Bread.find()
+      .then(foundBreads => {
+          res.render('index', {
+              breads: foundBreads,
+              title: 'Index Page'
+          })
+      })
 })
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  res.send(Bread[req.params.arrayIndex])
+breads.post('/', (req, res) => {
+  if(!req.body.image) {
+      req.body.image = undefined 
+  }
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.create(req.body)
+  res.redirect('/breads')
 })
+
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+      .then(foundBread => {
+          res.render('show', {
+              bread: foundBread
+          })
+      })
+})
+
+
 
 
 module.exports = breads
